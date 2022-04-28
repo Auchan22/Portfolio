c0a6d739f8b4f9e735553f84aac4c6eafa026f01
Agustin <agustinsurila@gmail.com>
2022-04-28 01:22:28 -0300

    Deleted some elements

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,17 +1,12 @@
-import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Page = () => {
   return (
     <Container minWidth={{base: 'auto', md: '60vw'}}>
-        <Box borderRadius='lg' fontWeight={'bold'} bg={useColorModeValue('red.500', '#3F3351')} mb={6} p={3} align='center'>
-            Bienvenido a mi Portfolio
-        </Box>
 
         <Box display={{md: 'flex'}}>
-            <Box flexGrow={1}>
-                <p>Desarrollador Fullstack MERN, enfocado al Frontend Development</p>
-            </Box>
+
 
             <Box flexShrink={0} my={{base: 4, md:0}} ml={{md:6}} align='center'>
                 <Image
@@ -26,7 +21,11 @@ const Page = () => {
                     src='https://via.placeholder.com/150x150'
                 />
             </Box>
+            <Box flexGrow={1}>
+                <Text textAlign={'center'} fontSize={20}>Desarrollador Fullstack MERN, enfocado al Frontend Development</Text>
+            </Box>
         </Box>
+
     </Container>
   )
 }
