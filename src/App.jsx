d2e51da9eaecc266ae392353600a8b700af71071
Agustin <agustinsurila@gmail.com>
2022-04-28 23:12:58 -0300

    Renderizado de acuerdo al intersection observer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
+import { useEffect } from 'react'
+
 import About from "./components/About"
 import Navbar from "./components/Navbar"
 import Page from "./components/Page"
+import { chakra } from '@chakra-ui/react'
+import { motion, isValidMotionProp, useAnimation } from 'framer-motion'
+import { useInView } from "react-intersection-observer"
+
+const variants = {
+  visible: {
+    opacity: 1, scale:1, transition:{duration: 1, delay: 0.5}, y: '-25px'
+  },
+  hidden: {
+    opacity: 0, scale: 1,
+  }
+}
+
+function AnimatedSection({children}){
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(()=>{
+    if(inView){
+      controls.start('visible')
+    }
+  },[controls, inView])
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={variants}
+      initial='hidden'
+      animate={controls} 
+    >
+      {children}
+    </motion.div>
+  )
+}
 
 function App() {
 
@@ -8,7 +44,11 @@ function App() {
     <>
       <Navbar/>
       <Page/>
-      <About />
+      <AnimatedSection children={<About />}/>
+      <AnimatedSection children={<About />}/>
+      <AnimatedSection children={<About />}/>
+      <AnimatedSection children={<About />}/>
+      <AnimatedSection children={<About />}/>
     </>
   )
 }
