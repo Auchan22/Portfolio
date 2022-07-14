5188fa9a1d4db0e531920f129f2dafdf69d93ada
Agustin <agustinsurila@gmail.com>
2022-07-14 19:06:56 -0300

    Modificando archivo vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public',
+  plugins: [react()],
+  server: {
+    host: true,
+  },
+});
