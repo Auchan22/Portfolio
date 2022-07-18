15e093c3e026d0bad6acbcca0fdf9314d768cc09
Agustin <agustinsurila@gmail.com>
2022-07-18 20:27:40 -0300

    Algunos cambios mas

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,9 +4,9 @@ import { FiGithub, FiExternalLink } from "react-icons/fi"
 
 const URL_IMG = "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
 
-const EffectImage = () =>{
+const EffectImage = ({src}) =>{
     return (
-        <Img alt="Imagén del proyecto" borderRadius='md' flex={1} src={URL_IMG} position={{base: "absolute", md: "relative"}} zIndex={{base: "-1", md: "0"}}  className={{base: null, md: styles.effectImg}} filter={{base: "auto", md: "auto"}} brightness={{base: "30%"}}  maxWidth="90vw" minHeight="400px" />
+        <Img alt="Imagén del proyecto" borderRadius='md' flex={1} src={src} position={{base: "absolute", md: "relative"}} zIndex={{base: "-1", md: "0"}}  className={{base: null, md: styles.effectImg}} maxWidth="400px" maxHeight="400px" display={{base: "none", md: "block"}}/>
     )
 }
 
@@ -34,14 +34,14 @@ const ProjectCard = ({data, position}) => {
     const [isLargerThan620] = useMediaQuery('(min-width: 620px)')
 
     return (
-        <Box maxWidth="50vw" minHeight="200px" margin={{base: "65px auto", md: "35px auto"}} display="flex" alignItems="center" justifyContent={{base: "center", md: "space-between"}}  key={data.id}>
+        <Box maxWidth="100%" minHeight={{base: "400px", md: "400px"}} margin="35px 0" display="flex" alignItems="center" justifyContent={{base: "center", md: "space-between"}} key={data.id} backgroundImage={{base: data.img, md: "none"}} backgroundSize="cover" backgroundPosition="center" backgroundRepeat="no-repeat" borderRadius="10px">
             {position === "left" ? 
             <>
-                <EffectImage query={isLargerThan620} />
-                <Box maxWidth="90vw" maxH="400px" ml={{base: 0, md: 5}} pl="2px" flex={1}>
+                <EffectImage src={data.img} />
+                <Box borderRadius="md" backdropFilter={{base: "auto", md: "none"}} backdropBlur="5px" backdropBrightness="30%" maxWidth="400px" minH={{base: "400px", md: "400px"}} ml={{base: 0, md: 2}} padding={{base: "10px", md: 0}} flex={1}>
                     <Text as="h5" color={{base: "#aafac8", md: useColorModeValue("#081c15", "#aafac8")}} className={styles.encabezadoProyectos}>Proyecto Seleccionado</Text>
                     <Heading as="h3" size="2xl" color={{base:"#f1faee" , md: useColorModeValue("#000000", "#f1faee")}}>{data.title}</Heading>
-                    <Text mt="10px" color={{base: "#b7b7a4", md: useColorModeValue("#081c15", "#b7b7a4")}} noOfLines={6} width={{base:"80vw", md: "20vw"}} maxHeight="200px">
+                    <Text mt="10px" color={{base: "#b7b7a4", md: useColorModeValue("#081c15", "#b7b7a4")}} mb="50px" width={{base:"80vw", md: "20vw"}} maxHeight="200px">
                         {data.desc}
                     </Text>
                     <List mt={5} flexWrap={{base: "wrap", md: "nowrap"}} display="flex" alignItems="center" textAlign="center">
@@ -54,10 +54,10 @@ const ProjectCard = ({data, position}) => {
             </>
              : 
              <>
-                <Box maxWidth="90vw" maxH="300px" flex={1} mr={{base: 0, md: 5}}>
+                <Box  padding={{base: "10px", md: 0}} backdropFilter={{base: "auto", md: "none"}} borderRadius="md" backdropBlur="5px" backdropBrightness="30%" maxWidth="400px" minH="400px" flex={1} mr={{base: 0, md: 2}}>
                     <Text as="h5" color={{base: "#aafac8", md: useColorModeValue("#081c15", "#aafac8")}} className={styles.encabezadoProyectos}>Proyecto Seleccionado</Text>
                     <Heading as="h3" size="2xl" color={{base:"#f1faee" , md: useColorModeValue("#000000", "#f1faee")}}>{data.title}</Heading>
-                    <Text mt="10px" color={{base: "#b7b7a4", md: useColorModeValue("#081c15", "#b7b7a4")}} noOfLines={6} width={{base:"80vw", md: "20vw"}} maxHeight="200px">
+                    <Text mt="10px" color={{base: "#b7b7a4", md: useColorModeValue("#081c15", "#b7b7a4")}} mb="50px" width={{base:"80vw", md: "20vw"}} maxHeight="200px">
                         {data.desc}
                     </Text>
                     <List mt={5} display="flex" flexWrap={{base: "wrap", md: "nowrap"}} alignItems="center" textAlign="center">
@@ -67,7 +67,7 @@ const ProjectCard = ({data, position}) => {
                     </List>
                     <Links l={data.links}/>
                 </Box>
-                <EffectImage />
+                <EffectImage src={data.img} />
             </>
              }
             
--- src/json/projects.js
+++ src/json/projects.js
@@ -3,7 +3,9 @@ export const PROJECTS = [
     id: 1,
     title: 'Blist-React',
     desc: 'Blist es una lista de supermercado digital, que te va a ayudar a anotar los productos que necesitas comprar para prepararle la cena a tu novia, o una juntada con tus amigos.',
-    img: '../../public/Logotipo_Blist.png',
+    img:
+      'https://agustinsurila.vercel.app/Logotipo_Blist.png' ||
+      'http://localhost:3000/Logotipo_Dolarsucho.png',
     links: [
       'https://github.com/Auchan22/Blist-React',
       'https://blist-shop.vercel.app/',
@@ -29,18 +31,19 @@ export const PROJECTS = [
     id: 2,
     title: 'Dolarsucho',
     desc: "Dolarsucho es una página web que te muestra las cotizaciones del dólar a valor peso Argentino. Fue un proyecto realizado para poner en práctica el consumo de API's, por medio de la API integrada en Javascript (fetch).",
-    img: '../../public/Logotipo_Dolarsucho.png',
-    links: [
-      'https://github.com/Auchan22/Dolarsucho',
-      'https://dolarsucho.netlify.app/',
-    ],
+    img:
+      'https://agustinsurila.vercel.app/Logotipo_Dolarsucho.png' ||
+      'http://localhost:3000/Logotipo_Dolarsucho.png',
+    links: ['https://github.com/Auchan22/Dolarsucho', ''],
     Tec: ['HTML', 'CSS', 'Javascript', 'Netlify'],
   },
   {
     id: 3,
     title: 'ChatsitoApp',
     desc: 'Pequeña aplicación desarrollada con tecnologías básicas, pero sumándole el uso de Firebase para el Hosting y almacenamiento de Datos. Chatsito App es una sala de chat, con loggeo por medio de tu correo electrónico, permitiéndote hablar con todos los integrantes de esa sala.',
-    img: '../../public/Logotipo_Chatsito.png',
+    img:
+      'https://agustinsurila.vercel.app/Logotipo_Chatsito.png' ||
+      'http://localhost:3000/Logotipo_Dolarsucho.png',
     links: ['', 'https://chatsitoapp.web.app/'],
     Tec: ['HTML', 'CSS', 'Javascript', 'Firebase'],
   },
